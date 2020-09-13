de419677b410c1f77de2db0b27477aafeb6121d3
zakarouf <zak.d.official@gmail.com>
2020-09-13 14:24:32 +0530

    updated engine to accept tilesets

---
+++ zse.h
@@ -0,0 +1,15 @@
+#ifndef ZTORG_MAIN_HEADER
+#define ZTORG_MAIN_HEADER
+
+#include "map_lib.h"
+#include "r_lib.h"
+#include "tile_lib.h"
+
+
+
+int zse_editor_st_main();
+int zse_tile_maker_main ();
+
+//void r_render2dworld(plotdata_t *chunk, int Xsize, int Ysize, int Zsize, int z);
+
+#endif
